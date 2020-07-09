f437f6a5cd41cba55cd9413c9954d68da60a5f49
Rail Kharisov <raven908@mail.ru>
2020-07-09 11:09:14 +0300

    V-01. Сверстать главную страницу сайта - слайдер с предложениями "компании"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
+
+import "./main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <nav className="nav">
+        <div className="nav__box"></div>
+        <div className="nav__lanSwitch">Eng</div>
+      </nav>
+      <section className="content">
+        <header className="content__header">
+          <a href="#" className="content__header__logo">
+            Need for drive
+          </a>
+          <span className="content__header__location">Ульяновск</span>
+        </header>
+        <main className="content__desc">
+          <div className="content__desc__title">
+            Каршеринг <br />
+            <span>Need for drive</span>
+          </div>
+          <p className="content__desc__text">
+            Поминутная аренда авто твоего города
+          </p>
+          <button className="content__desc__button">Забронировать</button>
+        </main>
+        <footer className="footer">
+          <span>© 2016-2019 «Need for drive»</span>8 (495) 234-22-44
+        </footer>
+      </section>
+      <section className="slider">
+        <div className="slider_1">
+          <div className="slider__title">Бесплатная парковка</div>
+          <p className="slider__text">
+            Оставляйте машину на платных городских парковках и разрешенных
+            местах, не нарушая ПДД, а также в аэропортах.
+          </p>
+          <button className="slider__button">Подробнее</button>
+        </div>
+      </section>
     </div>
   );
 }
