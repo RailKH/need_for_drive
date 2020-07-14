2805b14d13cfb6d447cf781a010f0b95376d1d01
Rail Kharisov <raven908@mail.ru>
2020-07-14 22:57:40 +0300

    V-02. Сверстать форму оформления заказа на аренду авто (шаг 1).

--- src/components/order.jsx
+++ src/components/order.jsx
@@ -25,17 +25,71 @@ class Order extends React.Component {
             <div className="order__content__location">
               <div className="form">
                 <div>
-                  <label for="city">City</label>
+                  <label for="city">Город</label>
                   <div>
-                    <input id="city" type="text"></input>
+                    <input
+                      id="city"
+                      type="text"
+                      placeholder="Начните вводить город..."></input>
                   </div>
+                </div>
+                <div>
+                  <label for="delivery_point">Пункт выдачи</label>
+                  <div>
+                    <input
+                      id="delivery_point"
+                      type="text"
+                      placeholder="Начните вводить пункт..."></input>
+                  </div>
+                </div>
+                <div className="map">
+                  <p>Выбрать на карте:</p>
+                  <div className="map__area"></div>
                 </div>
               </div>
             </div>
             <div className="order__content__model disabled"></div>
             <div className="order__content__extra disabled"></div>
             <div className="order__content__total disabled"></div>
-            <div className="order__content__cost">dfdfd</div>
+            <div className="order__content__cost">
+              <p className="title">Ваш заказ:</p>
+              <div className="param">
+                <div class="delivery_point">
+                  <span class="delivery_point__prop feature-left">
+                    Пункт выдачи
+                  </span>
+                  <span class="delivery_point__value feature-right">
+                    Ульяновск, Нариманова 42
+                  </span>
+                </div>
+                <div class="model">
+                  <span class="model__prop feature-left">Модель</span>
+                  <span class="model__value feature-right">Hyndai, i30 N</span>
+                </div>
+                <div class="color">
+                  <span class="color__prop feature-left">Цвет</span>
+                  <span class="color__value feature-right">Голубой</span>
+                </div>
+                <div class="duration">
+                  <span class="duration__prop feature-left">
+                    Длительность аренды
+                  </span>
+                  <span class="duration__value feature-right">1д 2ч</span>
+                </div>
+                <div class="rate">
+                  <span class="rate__prop feature-left">Тариф</span>
+                  <span class="rate__value feature-right">На сутки</span>
+                </div>
+                <div class="oil">
+                  <span class="oil__prop feature-left">Полный бак</span>
+                  <span class="oil__value feature-right">Да</span>
+                </div>
+              </div>
+              <p className="cost">
+                <span>Цена:</span> от 8 000 до 12 000 ₽
+              </p>
+              <button className="button disabled">Выбрать модель</button>
+            </div>
           </div>
         </section>
       </section>
