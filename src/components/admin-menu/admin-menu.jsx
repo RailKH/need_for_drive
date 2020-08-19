eaa6e8d1b4d73d90f8f08be3e61d7d4820f4623b
Rail Kharisov <raven908@mail.ru>
2020-08-19 10:39:56 +0300

    update

--- src/components/admin-menu/admin-menu.jsx
+++ src/components/admin-menu/admin-menu.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import "../admin-menu/admin-menu.scss";
 
 export default function MenuBlock() {
   return (
     <div className="menu-block">
       <div className="menu-block_logo admin_logo">Need for car</div>
-      <div className="menu-block_auto-cart"></div>
-      <div className="menu-block_auto-list"></div>
-      <div className="menu-block_order"></div>
-      <div className="menu-block_menu1"></div>
-      <div className="menu-block_menu2"></div>
+      <div className="menu-block_auto admin_cart">Карточка автомобиля</div>
+      <div className="menu-block_auto admin_list">Список авто</div>
+      <div className="menu-block_auto admin_order">Заказы</div>
+      <div className="menu-block_auto admin_menu1">Menu 4</div>
+      <div className="menu-block_auto admin_menu2">Menu 5</div>
     </div>
   );
 }
